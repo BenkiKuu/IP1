85589404caeaacbd22b73c0663ca6b3376e44a6e
Leo Igane <iganeleo@gmail.com>
2018-05-25 16:26:55 +0300

    creates a module.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { FormComponent } from './form/form.component';
+import { HighlightDirective } from './highlight.directive';
+import { DatePipe } from './date.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    FormComponent,
+    HighlightDirective,
+    DatePipe
   ],
   imports: [
     BrowserModule
