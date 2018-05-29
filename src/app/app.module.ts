a9cc604f6391fed182c6e44c6341b850ecd8d00c
Leo Igane <iganeleo@gmail.com>
2018-05-29 23:40:59 +0300

    finishes the function of the form without votes and highlight

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
-import { FormComponent } from './form/form.component';
+// import { FormComponent } from './form/form.component';
 import { HighlightDirective } from './highlight.directive';
 // import { DatePipe } from './date.pipe';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { TimeCountPipe } from './time-count.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
-    FormComponent,
+    // FormComponent,
     HighlightDirective,
     QuoteDetailComponent,
-    TimeCountPipe
+    TimeCountPipe,
+    QuoteFormComponent
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -7,12 +7,12 @@ import { Quote } from '../quote'
 })
 export class QuotesComponent implements OnInit {
     quotes = [
-        new Quote(1,'Watch Finding Nemo','Find an online version and watch merlin find his son',new Date(2018,3,14) ),
-        new Quote(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2018,6,9) ),
-        new Quote(3, 'Get new Phone Case','Diana has her birthday coming up soon',new Date(2018,1,12) ),
-        new Quote(4, 'Get Dog Food','Pupper likes expensive sancks',new Date(2018,0,18) ),
-        new Quote(5, 'Solve math homework','Damn Math',new Date(2018,2,14) ),
-        new Quote(6, 'Plot my world domination plan','Cause I am an evil overlord',new Date(2018,3,14) ),
+        new Quote(1, 'Admin','Do not let your Hearts be trobled, trust God and trust also in me.','Jesus Christ',new Date(2018,3,14) ),
+        new Quote(2, 'Admin','I am the way and the truth and the life, no one comes to the farther except through me.','Jesus Christ',new Date(2018,6,9) ),
+        new Quote(3, 'Admin','For what shall it profit a man if he gains the whole world, and lose his soul','Jesus Christ',new Date(2018,1,12) ),
+        new Quote(4, 'Admin','A new command I give you: love one another. As I have loved you, so you must love one another','Jesus Christ',new Date(2018,0,18) ),
+        new Quote(5, 'Admin','Blessed are the mercifull for they ill be shown mercy','Jesus Christ',new Date(2018,2,14) ),
+        new Quote(6, 'Admin','Let the one among you who is without sin be the first to cast a stone','Jesus Christ',new Date(2018,3,14) ),
 
     ]
     completeQuote(isComplete,index){
@@ -25,10 +25,14 @@ export class QuotesComponent implements OnInit {
             }
         }
     }
-  
-// this.quotes.splice(index,1);
-            // }
-          // }
+
+    addNewQuote(quote){
+            let quoteLength = this.quotes.length;
+            quote.id=quoteLength+1;
+            quote.completeDate = new Date(quote.completeDate)
+            this.quotes.push(quote)
+
+          }
     toogleDetails(index){
         this.quotes[index].showDescription = !this.quotes[index].showDescription;
       }
